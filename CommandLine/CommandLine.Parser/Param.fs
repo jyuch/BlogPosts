7d6f1cb14834c4987e71a1298f98324d45d41faa
jyuch <jyuch@users.noreply.github.com>
2018-07-25 21:28:51 +0900

    Add CommandLine

---
+++ CommandLine/CommandLine.Parser/Param.fs
@@ -0,0 +1,29 @@
+﻿namespace CommandLine.Parser
+
+open System
+
+type Param = { Level:int; IsDryRun:bool; OutputName:string option; Files:string list }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Param = 
+    
+    let rec parseImpl input param =
+            match input with
+            | "--level" :: x :: tail ->
+                match Int32.TryParse(x) with
+                | (true, x) when 1 <= x && x <= 5 -> parseImpl tail { param with Level = x }
+                | _ -> parseImpl tail param
+            | "--dry-run" :: tail -> 
+                parseImpl tail { param with IsDryRun = true }
+            | "--output-name" :: name :: tail ->
+                parseImpl tail { param with OutputName = Some(name) }
+            | head :: tail ->
+                parseImpl tail { param with Files = head :: param.Files }
+            | [] -> param
+
+    [<CompiledName("Parse")>]
+    let parse input =
+        parseImpl (Array.toList input) { Level = 1; IsDryRun = false; OutputName = None; Files = [] } 
+
+    [<CompiledName("Show")>]
+    let show x = printfn "%A" x
---
+++ CommandLine/CommandLine/Program.vb
@@ -0,0 +1,15 @@
+﻿Imports CommandLine.Parser
+
+Module Program
+
+    Sub Main()
+        Dim param1 = ParamModule.Parse({"--level", "2", "--dry-run", "c.txt"})
+        ParamModule.Show(param1)
+
+        Console.WriteLine()
+
+        Dim param2 = ParamModule.Parse({"--level", "2", "--output-name", "hoge.exe", "a.txt", "b.txt"})
+        ParamModule.Show(param2)
+    End Sub
+
+End Module
