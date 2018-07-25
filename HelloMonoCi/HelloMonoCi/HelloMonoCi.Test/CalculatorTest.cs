0a3015508f7a5d668d585d273cb30ef86d9796bd
jyuch <jyuch@users.noreply.github.com>
2018-07-25 20:52:41 +0900

    Add HelloMonoCi

---
+++ HelloMonoCi/HelloMonoCi/HelloMonoCi.Test/CalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace HelloMonoCi.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase(2,1,1)]
+        public void AddTest(int expected, int x, int y)
+        {
+            var calc  = new Calculator();
+            Assert.AreEqual(expected, calc.Add(x, y));
+        }
+    }
+}
