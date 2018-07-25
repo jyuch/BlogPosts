ee2cb5b9608ed1be34aa4c2acebb05f7f6011240
jyuch <jyuch@users.noreply.github.com>
2018-07-25 20:40:40 +0900

    Add Nullable

---
+++ Nullable/Nullable/Module1.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Module1
+
+    Sub Main()
+        Dim a = New Hoge() With {.Id = 1, .Name = "Hoge", .ParentId = 0}
+        Dim b = New Hoge() With {.Id = 1, .Name = "Hoge", .ParentId = Nothing}
+        Dim c As Hoge
+
+        Dim d = b.Ns(Function(it) it.Id)
+
+        Console.WriteLine(a.Ns(Function(it) it.Id))
+        Console.WriteLine(a.N(Function(it) it.Name))
+        Console.WriteLine(a.Ns(Function(it) it.ParentId))
+        
+        Console.WriteLine(b.Ns(Function(it) it.Id))
+        Console.WriteLine(b.N(Function(it) it.Name))
+        Console.WriteLine(b.Ns(Function(it) it.ParentId))
+
+        Console.WriteLine(c.Ns(Function(it) it.Id))
+        Console.WriteLine(c.N(Function(it) it.Name))
+        Console.WriteLine(c.Ns(Function(it) it.ParentId))
+    End Sub
+
+End Module
+
+Module NullableExtension
+
+    <Extension>
+    Public Function N(Of TModel, TResult As Class)(value As TModel, expr As Func(Of TModel, TResult)) As TResult
+        If value Is Nothing Then
+            Return Nothing
+        Else
+            Return expr(value)
+        End If
+    End Function
+
+    <Extension>
+    Public Function Ns(Of TModel, TResult As Structure)(value As TModel, expr As Func(Of TModel, TResult)) As Nullable(Of TResult)
+        If value Is Nothing Then
+            Return Nothing
+        Else
+            Return expr(value)
+        End If
+    End Function
+
+    <Extension>
+    Public Function Ns(Of TModel, TResult As Structure)(value As TModel, expr As Func(Of TModel, Nullable(Of TResult))) As Nullable(Of TResult)
+        If value Is Nothing Then
+            Return Nothing
+        Else
+            Return expr(value)
+        End If
+    End Function
+
+End Module
+
+Class Hoge
+    Public Property Id As Integer
+    Public Property Name As String
+    Public Property ParentId As Integer?
+End Class
---
+++ Nullable/Nullable/My Project/AssemblyInfo.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Nullable")>
+<Assembly: AssemblyProduct("Nullable")>
+<Assembly: AssemblyCopyright("Copyright (c) 2016 jyuch")>
+
+<Assembly: ComVisible(False)>
+<Assembly: CLSCompliant(True)>
+
+<Assembly: Guid("9469c8de-0614-42dd-bbe7-03bc84da7a89")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
