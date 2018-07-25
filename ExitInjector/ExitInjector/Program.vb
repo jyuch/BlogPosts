c9d3e1c5e7c3ed0cb4e7283765f18a4c345473ef
jyuch <jyuch@users.noreply.github.com>
2018-07-25 20:33:59 +0900

    Add ExitInjector

---
+++ ExitInjector/ExitInjector/Program.vb
@@ -0,0 +1,40 @@
+﻿Imports Mono.Cecil
+Imports Mono.Cecil.Cil
+
+Module Program
+
+    Sub Main(args As String())
+        Dim assmPath = args(0)
+        Dim limitDate = DateTime.Parse(args(1))
+
+        Console.WriteLine($"Target:{assmPath}")
+        Console.WriteLine($"Limit:{limitDate:yyyy/MM/dd}")
+
+        Inject(assmPath, limitDate.Year, limitDate.Month, limitDate.Day)
+    End Sub
+
+    Sub Inject(path As String, year As Integer, month As Integer, day As Integer)
+        Dim assm = AssemblyDefinition.ReadAssembly(path)
+        Dim nowPropertyGetter = assm.MainModule.Import(GetType(DateTime).GetProperty("Now").GetGetMethod())
+        Dim ctor = assm.MainModule.Import(GetType(DateTime).GetConstructor(New Type() {GetType(Integer), GetType(Integer), GetType(Integer)}))
+        Dim graterThan = assm.MainModule.Import(GetType(DateTime).GetMethod("op_GreaterThan", New Type() {GetType(DateTime), GetType(DateTime)}))
+        Dim envExit = assm.MainModule.Import(GetType(Environment).GetMethod("Exit", New Type() {GetType(Integer)}))
+
+        Dim entryPoint = assm.EntryPoint
+        Dim processor = entryPoint.Body.GetILProcessor()
+        Dim first = entryPoint.Body.Instructions(0)
+
+        processor.InsertBefore(first, processor.Create(OpCodes.Call, nowPropertyGetter))
+        processor.InsertBefore(first, processor.Create(OpCodes.Ldc_I4, year))
+        processor.InsertBefore(first, processor.Create(OpCodes.Ldc_I4, month))
+        processor.InsertBefore(first, processor.Create(OpCodes.Ldc_I4, day))
+        processor.InsertBefore(first, processor.Create(OpCodes.Newobj, ctor))
+        processor.InsertBefore(first, processor.Create(OpCodes.Call, graterThan))
+        processor.InsertBefore(first, processor.Create(OpCodes.Brfalse_S, first))
+        processor.InsertBefore(first, processor.Create(OpCodes.Ldc_I4_0))
+        processor.InsertBefore(first, processor.Create(OpCodes.Call, envExit))
+
+        assm.Write(path)
+    End Sub
+
+End Module
---
+++ ExitInjector/TargetApp/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        FizzBuzz(100)
+    End Sub
+
+    Sub FizzBuzz([end] As Integer)
+        For i = 1 To [end]
+            If i Mod 15 = 0 Then
+                Console.WriteLine("fizzbuzz")
+            ElseIf i Mod 5 = 0 Then
+                Console.WriteLine("buzz")
+            ElseIf i Mod 3 = 0 Then
+                Console.WriteLine("fizz")
+            Else
+                Console.WriteLine(i)
+            End If
+        Next
+    End Sub
+
+End Module
