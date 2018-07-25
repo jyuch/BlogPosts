237537ee8c12a933591b72cb4a786c44bf753782
jyuch <jyuch@users.noreply.github.com>
2018-07-25 21:19:56 +0900

    Add AkkaStreamTcp

---
+++ AkkaStreamTcp/AkkaStreamTcp/Program.cs
@@ -0,0 +1,55 @@
+﻿using Akka.Actor;
+using Akka.IO;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Tcp = Akka.Streams.Dsl.Tcp;
+
+namespace AkkaStreamTcp
+{
+    class Program
+    {
+        private static int i = 0;
+
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("akka-stream-tcp");
+            var materializer = system.Materializer();
+
+            Source<Tcp.IncomingConnection, Task<Tcp.ServerBinding>> connections =
+                system.TcpStream().Bind("localhost", 8888);
+
+            connections.RunForeach(connection =>
+            {
+                Console.WriteLine($"New connection from: {connection.RemoteAddress}");
+
+                var echo = Flow.Create<ByteString>()
+                    .Via(Framing.Delimiter(
+                        ByteString.FromString("\n"),
+                        maximumFrameLength: 256,
+                        allowTruncation: true))
+                    .Select(c => c.ToString())
+                    .SelectAsync(1, Sum)
+                    .Select(ByteString.FromString);
+
+                connection.HandleWith(echo, materializer);
+            }, materializer);
+
+            Console.ReadLine();
+        }
+
+        static async Task<string> Sum(string value)
+        {
+            if (int.TryParse(value, out int n))
+            {
+                Interlocked.Add(ref i, n);
+            }
+
+            await Task.Delay(1000);
+            return $"{i,-11}";
+        }
+    }
+}
---
+++ AkkaStreamTcp/AkkaStreamTcpClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using Akka.Actor;
+using Akka.Routing;
+using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AkkaStreamTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("client");
+            var props = Props.Create<ClientActor>().WithRouter(new BroadcastPool(100)).WithDispatcher("default-fork-join-dispatcher");
+            var actor = system.ActorOf(props, "client");
+
+            for (var i = 0; i < 100; i++)
+            {
+                actor.Tell(10);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ClientActor : ReceiveActor
+    {
+        private Guid id;
+
+        public ClientActor()
+        {
+            id = Guid.NewGuid();
+
+            ReceiveAsync<int>(async i =>
+            {
+                var sw = Stopwatch.StartNew();
+
+                using (var client = new TcpClient("localhost", 8888))
+                using (var stream = client.GetStream())
+                {
+                    var message = Encoding.ASCII.GetBytes($"{i,-11}\n");
+                    await stream.WriteAsync(message, 0, message.Length);
+                    await stream.FlushAsync();
+                    var response = new byte[11];
+                    await stream.ReadAsync(response, 0, 11);
+                    var n = Encoding.ASCII.GetString(response);
+                    Console.WriteLine($"{id} {n} {sw.ElapsedMilliseconds}");
+                }
+            });
+        }
+    }
+}
