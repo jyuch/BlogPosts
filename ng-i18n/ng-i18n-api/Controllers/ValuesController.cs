b27964dd93164fda2d6c7613eb805ce70684410d
jyuch <jyuch@users.noreply.github.com>
2019-05-04 21:03:42 +0900

    Add ng-i18n-api

---
+++ ng-i18n/ng-i18n-api/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ng_i18n_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<TValue> Get()
+        {
+            return new TValue { Value = 0 };
+        }
+    }
+
+    public class TValue
+    {
+        public int Value { set; get; }
+    }
+}
